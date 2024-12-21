1f10af5a7e9b7137b9c87e0f16b8289db8f1371c
juliabybee <106109681+juliabybee@users.noreply.github.com>
2024-12-21 13:07:19 -0600

    Delete spcwdFinal/spaceward/spaceward2 directory

--- spcwdFinal/spaceward/spaceward2/src/main/java/com/example/spaceward2/repository/ScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.spaceward2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.spaceward2.model.Schedule;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
-}
-
